b7c346d353b9dbc5fbb95fe8cfcb8900e933dc99
Chase Starr <chasestarr@gmail.com>
2018-09-01 17:18:00 -0700

    init

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sass',
+  ],
+}
