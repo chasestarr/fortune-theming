fad5f2aeb96b1026bcfa45a40c9887937ce64662
Chase Starr <chasestarr@gmail.com>
2018-09-22 14:16:14 -0700

    alert block

---
+++ src/pages/components.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import Block from '../components/block'
+import ColorPicker from '../components/colorPicker'
+import NumberPicker from '../components/numberPicker'
+import { getCustomProperty } from '../utils'
+
+export default () => (
+  <div>
+    <Block
+      nav="alert"
+      title="Alert"
+      demo={() => (
+        <div>
+          <p className="code pad1">
+            {`<div className="alert">Default</div>`}
+            <br />
+            {`<div className="alert bgPrimary">Primary</div>`}
+            <br />
+            {`<div className="alert bgSuccess">Success</div>`}
+            <br />
+            {`<div className="alert bgWarning">Warning</div>`}
+          </p>
+          <div className="alert">Default</div>
+          <div className="alert bgPrimary">Primary</div>
+          <div className="alert bgSuccess">Success</div>
+          <div className="alert bgWarning">Warning</div>
+        </div>
+      )}
+      editor={() => (
+        <div>
+          <NumberPicker name="--alert-border-radius" />
+          <NumberPicker name="--alert-padding" />
+          <ColorPicker name="--alert-color" />
+          <ColorPicker
+            name="--alert-bg-color"
+            fallbackValue={getCustomProperty('--state-primary')}
+          />
+        </div>
+      )}
+    />
+  </div>
+)
