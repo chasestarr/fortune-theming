1c37f7d8f2fe244cb564816e3f3d35498bed0b9c
Chase Starr <chasestarr@gmail.com>
2018-09-22 12:33:23 -0700

    adds left menu

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/header'
 import './index.scss'
 
 const Layout = ({ children, data }) => (
@@ -14,8 +13,60 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div>{children()}</div>
+    <div className="cols isFull">
+      <div className="col2 bgSmoke padX3">
+        <ul className="sticky top2">
+          <li>
+            <a href="/" className="h3">
+              🔮
+            </a>
+          </li>
+          <li className="marB4 isGap">
+            <a href="/#introduction" className="gray bold">
+              Introduction
+            </a>
+          </li>
+          <li className="marL4 marB4 isGap">
+            <a href="/#install" className="gray bold">
+              Install
+            </a>
+          </li>
+          <li className="marL4 marB4 isGap">
+            <a href="/#theming" className="gray bold">
+              Theming
+            </a>
+          </li>
+          <li className="marL4 marB4 isGap">
+            <a href="/#variables" className="gray bold">
+              Variables
+            </a>
+          </li>
+          <li className="marB4 isGap">
+            <a href="/layout" className="gray bold">
+              Layout
+            </a>
+          </li>
+
+          <li className="marB4 isGap">
+            <a href="/components" className="gray bold">
+              Components
+            </a>
+          </li>
+          <li className="marB4 isGap">
+            <a href="/utilities" className="gray bold">
+              Utilities
+            </a>
+          </li>
+
+          <li className="marB4 isGap">
+            <a href="https://github.com/atav1k/fortune" className="gray bold">
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col10">{children()}</div>
+    </div>
   </div>
 )
 
