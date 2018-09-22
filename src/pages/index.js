9d490a0a40c83c5cff988effb5db42206f72d807
Chase Starr <chasestarr@gmail.com>
2018-09-22 13:12:56 -0700

    set index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,74 +1,40 @@
 import React from 'react'
 
-import { states } from '../constants'
+import Block from '../components/block'
 
-import Knobs from '../components/knobs'
-
-const IndexPage = () => (
-  <div className="cols">
-    <div className="col4">
-      <Knobs />
-    </div>
-    <div className="col8">
-      <h3>Alert</h3>
-      <div>
-        <div className="alert">Default</div>
-        {states.map(state => (
-          <div className={`alert bg${state.upper}`}>{state.upper}</div>
-        ))}
-      </div>
-
-      <h3>Border</h3>
-      <div>
-        <div className="bord0 marB1">.bord0</div>
-        <div className="bord1 marB1">.bord1</div>
-        <div className="bord2 marB1">.bord2</div>
-        <div className="bordWid marB2">.bordWid</div>
-
-        {states.map(state => (
-          <div className={`bord1 bord${state.upper} marB1`}>{state.upper}</div>
-        ))}
-      </div>
-
-      <h3>Button</h3>
-      <div>
-        {states.map(state => {
-          return (
-            <div className="marB1">
-              <button className={`bg${state.upper} bord${state.upper} marR1`}>
-                {state.upper}
-              </button>
-              <button
-                className={`bg${state.upper} bord${state.upper} isActive marR1`}
-              >
-                Active
-              </button>
-              <button
-                className={`${state.lower} bg${state.upper} bord${
-                  state.upper
-                } isOutline marR1`}
-              >
-                Outline
-              </button>
-              <button
-                disabled
-                className={`bg${state.upper} bord${state.upper}`}
-              >
-                Disabled
-              </button>
-            </div>
-          )
-        })}
-      </div>
-
-      <h3>Chip</h3>
-      <div>
-        <a href="https://github.com/atav1k/fortune" className="chip">
-          chip
-        </a>
-      </div>
-    </div>
+export default () => (
+  <div>
+    <Block
+      nav="introduction"
+      title="Introduction"
+      body={() => (
+        <p className="h4">
+          Fortune.css is a friendly CSS variables driven library. It is light,
+          customizable and needs no preprocessor builds.
+        </p>
+      )}
+      demo={() => (
+        <div>
+          <p>
+            Fortune is well suited to static sites and code pens, and built to
+            support new CSS specs using current style and naming conventions.
+          </p>
+          <div className="cols">
+            <ul className="col">
+              <li className="bold">Benefits</li>
+              <li>🌈 Uses CSS custom properties</li>
+              <li>⚡️ Light-weight at ~ 10kb gzipped</li>
+              <li>⛔️ No preprocessor required</li>
+            </ul>
+            <ul className="col">
+              <li className="bold">Features</li>
+              <li>📐 Supports CSS grids and flexbox</li>
+              <li>🥊 Non-blocking utility classes</li>
+              <li>💧 Essential components to start</li>
+            </ul>
+          </div>
+        </div>
+      )}
+    />
   </div>
 )
-
-export default IndexPage
